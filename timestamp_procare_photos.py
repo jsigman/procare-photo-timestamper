0fcb1ed01f71b0e8338fca84ca993d049d47d1c7
John Sigman <john.sigman@gmail.com>
2025-01-10 06:42:35 -0500

    updates for new timestamping on procare backend

--- timestamp_procare_photos.py
+++ timestamp_procare_photos.py
@@ -4,7 +4,7 @@ import sys
 from bdb import BdbQuit
 from pathlib import Path
 from typing import List
-
+import re
 import pytz
 from dateutil.tz import tzlocal
 from PIL.ExifTags import TAGS
@@ -31,6 +31,13 @@ def _get_offset_time(timestamp: datetime.datetime) -> str:
     offset_hours = -1 * (offset.seconds // 3600)
     return f"{offset_hours:02d}:00"
 
+def extract_first_img_number(text) -> str:
+    pattern = r'img_(\d+)_photo'  # captures digits between img_ and _photo
+    match = re.search(pattern, text)
+    if match:
+        return match.group(1)
+    else:
+        raise ValueError(f"Could not find img number in {text}")
 
 def _timestamp_to_exif_string(timestamp: datetime.datetime) -> str:
     return timestamp.strftime("%Y:%m:%d %H:%M:%S")
@@ -39,14 +46,16 @@ def _timestamp_to_exif_string(timestamp: datetime.datetime) -> str:
 def _get_timestamp_from_filename(filepath: Path) -> datetime.datetime:
     file_stem = filepath.stem
 
-    # there should only be one of these
-    number_string = next(x for x in file_stem.split("_") if x.isdigit())
+    # Take the first one if there are multiple
+    number_string = extract_first_img_number(file_stem)
 
     num_digits = len(number_string)
-    assert num_digits > 10
 
-    num_decimal_points = num_digits - 10
-    unix_time = int(number_string) / (10**num_decimal_points)
+    if num_digits > 10:
+        num_decimal_points = num_digits - 10
+        unix_time = int(number_string) / (10**num_decimal_points)
+    else:
+        unix_time = int(number_string)
     timestamp = datetime.datetime.fromtimestamp(unix_time)
 
     # sanity check
@@ -86,8 +95,8 @@ def main(photos_dir: str):
     photos_dir = Path(photos_dir).resolve()
     assert photos_dir.exists()
 
-    photo_paths = _get_image_paths(photos_dir, "img_*_photo.jpg")
-    activity_paths = _get_image_paths(photos_dir, "img_*_activity.jpg")
+    photo_paths = _get_image_paths(photos_dir, "*img_*_photo*jpg")
+    activity_paths = _get_image_paths(photos_dir, "*img_*_activity.jpg*")
     all_image_paths = photo_paths + activity_paths
     
     for image_path in all_image_paths:
